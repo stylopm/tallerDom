7d694a97e08af84c6646cb5a52c066754a407d20
stylopm <apmurillo99@hotmail.com>
2022-09-29 21:18:09 -0500

    feat: Update form

--- localstorage/script.js
+++ localstorage/script.js
@@ -1,80 +1,84 @@
-let mostrarAlumnos = () => {
-    let alumnosLS = JSON.parse(localStorage.getItem('alumnosls'))
-    if( alumnosLS === null){
-        console.log('entro');
-        localStorage.setItem('alumnosls',  JSON.stringify([]));
-        alumnosLS = []
+// Variables globales
+let nameUser = document.getElementById("nameUser");
+let age = document.getElementById("age");
+let indexForm = document.getElementById("indexForm");
+
+const agregarRegistro = () => {
+  // Leo lo que hay en memoria
+  if (validation()) {
+    let listAlum = JSON.parse(localStorage.getItem("listAlumn"));
+    if (indexForm.value === ''){
+        if (listAlum === null) {
+        listAlum = [];
+        }
+        const newUser = {
+        name: nameUser.value,
+        age: age.value,
+        };
+        listAlum.push(newUser);
+        localStorage.setItem("listAlumn", JSON.stringify(listAlum));
+    } else {
+        console.log('Editar');
+        listAlum[indexForm.value].name = nameUser.value;
+        listAlum[indexForm.value].age = age.value;
+        document.getElementById('btnAction').innerText='Agregar'
+        localStorage.setItem("listAlumn", JSON.stringify(listAlum));
     }
-    document.getElementsByTagName("tbody")[0].innerHTML = '';
-    alumnosLS.forEach((element, index) => {
-        let texth1 = document.createElement("tr");
-        texth1.innerHTML = `
-        <td scope="row">${index + 1}</td>
-        <td>${element.name}</td>
-        <td>${element.age}</td>
-        <td>
-            <button type="button" class="btn btn-primary" onclick="rellenarFormulario(${index})">
-                Editar
-            </button>
-            <button type="button" class="btn btn-danger" onclick="eliminarRegistro(${index})">
-                Eliminar
-            </button>
-        </td>`;
-        document.getElementsByTagName("tbody")[0].appendChild(texth1);
-    });
+    clearForm();
+    showTable();
+  }
+};
+
+const showTable = () => {
+  let listAlum = JSON.parse(localStorage.getItem("listAlumn"));
+  if (listAlum === null) {
+    listAlum = [];
+  }
+  document.getElementById("tbodyTablet").innerHTML = "";
+  let row = "";
+  listAlum.forEach((element, index) => {
+    row += `<tr>
+                    <td>${index + 1}</td>
+                    <td>${element.name}</td>
+                    <td>${element.age}</td>
+                    <td>
+                    <button class="btn btn-primary" onClick="search(${index})">Editar</button>
+                    <button class="btn btn-danger" onClick="deleteItem(${index})">Eliminar</button></td>
+                </tr>
+            `;
+  });
+  document.getElementById("tbodyTablet").innerHTML = row;
+};
+
+const clearForm = () => {
+  nameUser.value = "";
+  age.value = "";
+  indexForm.value = "";
+};
+
+const validation = () => {
+  if (nameUser.value === "") {
+    alert("Debe digitar el nombre");
+    return false;
+  }
+  if (age.value === "") {
+    alert("Debe digitar el edad");
+    return false;
+  }
+  return true;
+};
+
+const search = (index) =>{
+    let listAlum = JSON.parse(localStorage.getItem("listAlumn"));
+    nameUser.value = listAlum[index].name;
+    age.value = listAlum[index].age;
+    indexForm.value = index;
+    document.getElementById('btnAction').innerText='Editar'
 }
 
-let eliminarRegistro = (parIndex) => {
-    alumnosAlmacenados = JSON.parse(localStorage.getItem('alumnosls'))
-    alumnosAlmacenados = alumnosAlmacenados.filter((alumno, index) => index !== parIndex);
-    localStorage.setItem('alumnosls',  JSON.stringify(alumnosAlmacenados));
-    mostrarAlumnos();
+const deleteItem = (index) =>{
+    console.log(index);
+
 }
 
-let rellenarFormulario = (parIndex) => {
-    alumnosAlmacenados = JSON.parse(localStorage.getItem('alumnosls'))
-    document.getElementById('position').value = parIndex;
-    document.getElementById('nameUser').value = alumnosAlmacenados[parIndex].name;
-    document.getElementById('age').value = alumnosAlmacenados[parIndex].age;
-}
-
-let actualizarRegistro = () => {
-    let position = document.getElementById('position').value;
-    let nameUser = document.getElementById('nameUser').value;
-    let age = document.getElementById('age').value;
-    if (position == '' || isNaN(position)) {
-        alert('Debe seleccionar un registro');
-        return false;
-    }
-    alumnosAlmacenados = JSON.parse(localStorage.getItem('alumnosls'))
-    alumnosAlmacenados[position] = {
-        name: nameUser,
-        age: age
-    }
-    document.getElementById('position').value = '';
-    document.getElementById('nameUser').value = '';
-    document.getElementById('age').value = '';
-    localStorage.setItem('alumnosls',  JSON.stringify(alumnosAlmacenados));
-    mostrarAlumnos();
-}
-
-let agregarRegistro = () => {
-    alumnosLS = JSON.parse(localStorage.getItem('alumnosls'))
-    const nuevoAlumno = {
-        name: document.getElementById('nameUser').value,
-        age: document.getElementById('age').value
-    }
-    document.getElementById('nameUser').value = '';
-    document.getElementById('age').value = '';
-    alumnosLS.push(nuevoAlumno);
-    localStorage.setItem('alumnosls',  JSON.stringify(alumnosLS));
-    mostrarAlumnos();
-}
-
-let clearCache = () => {
-    localStorage.clear();    
-    alert('Se limpio la memoria de localstorage');
-    mostrarAlumnos();
-}
-
-mostrarAlumnos();
+showTable();
